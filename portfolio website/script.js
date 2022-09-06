a24c965fa69f0b7ee9c690b436494b8f9312319d
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2022-09-06 18:40:00 +0800

    Update script.js

--- portfolio website/script.js
+++ portfolio website/script.js
@@ -1,24 +1,38 @@
-var express = require('express'),
-    cors = require('cors'),
-    secure = require('ssl-express-www');
-const PORT = process.env.PORT || 8080 || 5000 || 3000
-var { color } = require('./lib/color.js')
+$(document).ready(function(){
 
-var mainrouter = require('./routes/main'),
-    apirouter = require('./routes/api')
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
 
-var app = express()
-app.enable('trust proxy');
-app.set("json spaces",2)
-app.use(cors())
-app.use(secure)
-app.use(express.static("public"))
+  $(window).on('scroll load',function(){
 
-app.use('/', mainrouter)
-app.use('/api', apirouter)
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
 
-app.listen(PORT, () => {
-    console.log(color("Server running on port " + PORT,'green'))
-})
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
 
-module.exports = app
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
